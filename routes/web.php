0808e1a5a8e4530a4ff3b30c8cf314d5f2bf3b3c
Segawa Shoko <shoko4prog@gmail.com>
2020-06-13 04:36:12 +0900

    prefix削除

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::prefix('/questionnaire_bbs')->group(function () {
-    Route::get('/','pagesController@index');
-    Route::get('/logout','pagesController@logout');
-    Route::get('/home', 'pagesController@index');
-    Route::post('/delete','pagesController@delete');
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/mypage','pagesController@mypage');
-        Route::get('/makeQ','pagesController@makeQ');
-        Route::post('/sendQ','pagesController@sendQ');
-        Route::get('/detail','pagesController@detail');
-        Route::post('/submit','pagesController@submit');
-    });
-    Auth::routes();
+
+Route::get('/','pagesController@index');
+Route::get('/logout','pagesController@logout');
+Route::get('/home', 'pagesController@index');
+Route::post('/delete','pagesController@delete');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/mypage','pagesController@mypage');
+    Route::get('/makeQ','pagesController@makeQ');
+    Route::post('/sendQ','pagesController@sendQ');
+    Route::get('/detail','pagesController@detail');
+    Route::post('/submit','pagesController@submit');
 });
+Auth::routes();
+
 
