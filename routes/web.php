2adb468ae6d074dd9b9a37fd3f91fe267773b8e5
Segawa Shoko <shoko4prog@gmail.com>
2020-06-14 04:40:39 +0900

    test commit

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/welcome','pagesController@welcome');
     Route::get('/register_complete','pagesController@welcome');
 });
+
 Auth::routes();
 
 
