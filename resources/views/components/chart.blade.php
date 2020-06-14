a174d91830cc1167c83d8ee0324cef1c1d2c7420
Segawa Shoko <shoko4prog@gmail.com>
2020-06-15 01:02:00 +0900

    ツールチップの変更とアニメーションの削除

--- resources/views/components/chart.blade.php
+++ resources/views/components/chart.blade.php
@@ -74,25 +74,26 @@ Chart.plugins.register({
         legend: {
           display: false,
         },
-        tooltips:{
-          callbacks: {
-            label: function(tooltipItem, data) {
-              //get the concerned dataset
-              var dataset = data.datasets[tooltipItem.datasetIndex];
-              //calculate the total of this data set
-              var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-                return previousValue + currentValue;
-              });
-              //get the current items value
-              var currentValue = dataset.data[tooltipItem.index];
-              //calculate the percentage based on the total and current item, also this does a rough rounding to give a whole number
-              var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+        // tooltips:{
+        //   callbacks: {
+        //     label: function(tooltipItem, data) {
+        //       //get the concerned dataset
+        //       var dataset = data.datasets[tooltipItem.datasetIndex];
+        //       //calculate the total of this data set
+        //       var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+        //         return previousValue + currentValue;
+        //       });
+        //       //get the current items value
+        //       var currentValue = dataset.data[tooltipItem.index];
+        //       //calculate the percentage based on the total and current item, also this does a rough rounding to give a whole number
+        //       var percentage = Math.floor(((currentValue/total) * 100)+0.5);
 
-              return percentage + '%';
-            }
-          },
-        },
-        maintainAspectRatio: false
+        //       return percentage + '%';
+        //     }
+        //   },
+        // },
+        animation:false,
+        maintainAspectRatio: false,
       }
     });
 </script>
